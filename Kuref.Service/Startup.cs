26f30563651c306eda9a8c66ac02d7fac479a922
Diego Cid <diego.cid@outlook.com>
2020-07-05 14:18:54 -0400

    Add xml documentation

--- Kuref.Service/Startup.cs
+++ Kuref.Service/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Kuref.Service.Data;
 using Microsoft.AspNetCore.Builder;
@@ -31,7 +33,12 @@ namespace Kuref.Service
             services.AddDbContext<KurefContext>(options =>
             options.UseNpgsql(Configuration.GetConnectionString("KurefContext")));
 
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(c =>
+            {
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +54,7 @@ namespace Kuref.Service
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Kuref Measurements API V1");
+                
             });
 
             app.UseHttpsRedirection();
